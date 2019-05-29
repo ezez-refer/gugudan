3ba2e95770f14984c6b9d9f27d8fd9c30b82e103
JeongJunNyeong <JeongJunNyeong@192.168.1.4>
2019-05-29 16:37:07 +0900

    배열, 문자열 입력 연습

--- src/gugudan1.java
+++ src/gugudan1.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 
-public class gugudan {
+public class gugudan1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,12 +16,41 @@ public class gugudan {
 		System.out.println("2*9 = 18");
 		*/
 		
-		for(int dan = 2; dan <= 9; dan++) {
-			for(int num = 1; num <= 9; num++) {
-				System.out.println(dan + "*" + num + "=" + dan*num);
-			}
-			System.out.println();
+//		for(int dan = 2; dan <= 9; dan++) {
+//			for(int num = 1; num <= 9; num++) {
+//				System.out.println(dan + "*" + num + "=" + dan*num);
+//			}
+//			System.out.println();
+//		}
+//		System.out.println("완성~ 깃허브는 이렇게 쓰는거구나!");
+		
+		// 입력받기
+//		int dan;
+//		int num;
+//		
+//		System.out.print("몇 단을 볼까? : ");
+//		Scanner sc = new Scanner(System.in);
+//		dan = sc.nextInt();
+//		
+//		for(num = 1; num <= 9; num++) 
+//			System.out.println(dan + "*" + num + "=" + dan*num);
+//		System.out.println();
+		
+		//배열
+		int[] result = new int[10];
+		for(int i = 0; i < result.length; i++)	{
+			result[i] = 2*(i+1);
 		}
-		System.out.println("완성~ 깃허브는 이렇게 쓰는거구나!");
+		for(int i=0; i<result.length; i++) {
+			System.out.println(result[i]);
+		}
+		
+		//문자열 입력
+		System.out.print("입력 : ");
+		String m;
+		Scanner sc = new Scanner(System.in);
+		m = sc.nextLine();
+		
+		System.out.print("jjn : " + m);
 	}
 }
