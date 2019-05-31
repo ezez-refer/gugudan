7a7b0530f71df3faa00d39bf69511f8d205778ad
JeongJunNyeong <JeongJunNyeong@192.168.1.4>
2019-05-31 15:29:27 +0900

    나름대로 설명 추가

--- src/Gugudan1.java
+++ src/Gugudan1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Gugudan1 {
 
 	// 메소드
-	public static int[] Mcalculate(int dan, int times) {
+	public static int[] Mcalculate(int dan, int times) {	// 계산하는 메소드
 		int[] result = new int[times];
 		for (int i = 0; i < result.length; i++) {
 			result[i] = dan * (i + 1);
@@ -11,13 +11,13 @@ public class Gugudan1 {
 		return result;
 	}
 
-	public static void Mprint(int[] result) {
+	public static void Mprint(int[] result) {			// 출력하는 메소드
 		for (int i = 0; i < result.length; i++) {
 			System.out.println(result[i]);
 		}
 	}
 	
-	public static String Mscan() {
+	public static String Mscan() {						// 입력받는 메소드
 
 		System.out.print("몇 단, 몇 회 ? (X,Y 로 입력) : ");
 		Scanner scan = new Scanner(System.in);
