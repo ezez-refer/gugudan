ec28dbde6b8d901e565e763f0a1e40610fd6b052
JeongJunNyeong <JeongJunNyeong@192.168.1.4>
2019-05-31 15:28:43 +0900

    나름대로 설명 추가

--- src/Gugudan1main.java
+++ src/Gugudan1main.java
@@ -13,11 +13,11 @@ public class Gugudan1main {
 //		int[] dan5 = calculate(5);
 //		print(dan5);
 
-		String inputValue = Gugudan1.Mscan();
-		String[] splitedValue = inputValue.split(",");
-		int dan = Integer.parseInt(splitedValue[0]);
-		int times = Integer.parseInt(splitedValue[1]);
-		for (int i = 2; i <= dan; i++) {
+		String inputValue = Gugudan1.Mscan();				// 사용자에게 입력값 받기
+		String[] splitedValue = inputValue.split(",");		// 사용자에게 입력 받은 값을 ,를 기준으로 차례로 배열로 저장
+		int dan = Integer.parseInt(splitedValue[0]);		// splitedValue 첫 번째 배열값을 int로 변환하여 저장
+		int times = Integer.parseInt(splitedValue[1]);		// splitedValue 두 번째 배열값을 int로 변환하여 저장
+		for (int i = 2; i <= dan; i++) {					// 메소드 활용하여 결과 출력
 			int[] result = Gugudan1.Mcalculate(i, times);
 			Gugudan1.Mprint(result);
 		}
